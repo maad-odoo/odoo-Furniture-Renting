45cf7166a45346fffef46696bbaff4adf3271201
yashvardhan8787 <139737436+yashvardhan8787@users.noreply.github.com>
2024-06-29 09:26:14 +0530

    add  coponent

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './components/Login/Login';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-<div className=' text-blue-500'>
-  APP
-</div>
+    <div>
+      <Login/>
+    </div>
     </>
   )
 }
