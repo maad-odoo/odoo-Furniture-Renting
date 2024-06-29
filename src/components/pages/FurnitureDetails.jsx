34b5371e62c48293cc8158ab1afa2406f7c65579
matinshaikh31 <118182648+matinshaikh31@users.noreply.github.com>
2024-06-29 17:24:15 +0530

    BuyNow BTN

--- src/components/pages/FurnitureDetails.jsx
+++ src/components/pages/FurnitureDetails.jsx
@@ -1,4 +1,3 @@
-// pages/FurnitureDetails.js
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useFireBase } from '../../context/FireBase';
@@ -33,14 +32,15 @@ const FurnitureDetails = () => {
     }
 
     return (
-        
-        <div className="min-h-screen bg-white flex items-center justify-center"
-        style={{
-            backgroundImage: `url(${itemData.images})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }} >
-            <div className="max-w-2xl w-full bg-amber-200 p-8 rounded-lg shadow-lg" style={{ opacity: 0.7 }}>
+        <div
+            className="min-h-screen bg-white flex items-center justify-center"
+            style={{
+                backgroundImage: `url(${itemData.images})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
+            <div className="max-w-2xl w-full bg-amber-200 p-8 rounded-lg shadow-lg mt-10 mb-10" style={{ opacity: 0.7 }}>
                 <img src={itemData.images} alt={itemData.name} className="w-full h-64 object-cover rounded-md mb-6" />
                 <h2 className="text-2xl font-bold text-amber-800 mb-4">{itemData.name}</h2>
                 <p className="text-amber-800 mb-2">Price: ${itemData.price}</p>
@@ -50,9 +50,14 @@ const FurnitureDetails = () => {
                 <p className="text-amber-800 mb-2">Color: {itemData.color}</p>
                 <p className="text-amber-800 mb-2">Availability: {itemData.availability}</p>
                 <p className="text-amber-800 mb-2">Description: {itemData.description}</p>
+
+                <div className="flex justify-center">
+                    <button className="bg-amber-800 text-amber-200 px-4 py-2 rounded-md hover:bg-amber-700 focus:outline-none focus:ring-2 focus:ring-amber-800">
+                        Buy Now
+                    </button>
+                </div>
             </div>
         </div>
-    
     );
 };
 
